9db4dc9ecb375b7df7f9e4ab74b7f3de13bf76b6
Jewel Hasan <jewel.hasan@cg-bd.com>
2025-05-05 10:11:21 +0600

    final

--- src/pages/Home/Home/Testimonial/Testimonial.jsx
+++ src/pages/Home/Home/Testimonial/Testimonial.jsx
@@ -11,7 +11,7 @@ import { Rating } from "@smastrom/react-rating";
 const Testimonial = () => {
   const [reviews, setReviews] = useState([]);
   useEffect(() => {
-    fetch(`${process.env.VITE_baseURL}/reviews`)
+    fetch(`${import.meta.env.VITE_baseURL}/reviews`)
       .then((res) => res.json())
       .then((data) => setReviews(data));
   }, []);
